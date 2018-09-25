cef8d32a0f188c257ec19ac095a65d0d7a32b667
Vivo Xu <vivo@fancyber.com>
2018-09-24 23:23:20 -0700

    first version of installer

---
+++ README.md
@@ -0,0 +1,15 @@
+# PortPi Installer
+
+This is the installer of PortPi.
+
+## Prerequisite
+
+Wired Internet connection. (DO NOT use wireless Internet!)
+
+## Usage
+
+Just run:
+
+```bash
+curl -sL https://raw.githubusercontent.com/portpi/installer/master/install.sh | sudo -E bash -
+```
--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+# Install Git and NodeJS first
+# It will run apt-get update
+
+curl -sL https://deb.nodesource.com/setup_8.x | bash
+apt-get install -y git nodejs
+
 # Install required packages
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
+apt-get install -y dnsmasq hostapd
 
-sudo apt-get install -y dnsmasq hostapd
-
-sudo systemctl stop dnsmasq
-sudo systemctl stop hostapd
+systemctl stop dnsmasq
+systemctl stop hostapd
 
 # Configure a static IP
 
 if ! grep -q '^interface wlan0$' /etc/dhcpcd.conf; then
-  sudo tee -a /etc/dhcpcd.conf > /dev/null << EOD
+  cat << EOD >> /etc/dhcpcd.conf
 
 # static ip for PortPi
 interface wlan0
@@ -23,12 +26,12 @@ EOD
 fi
 
 # TODO consider to move this line to the last
-sudo service dhcpcd restart
+service dhcpcd restart
 
 # Configure DHCP server
 
-sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
-sudo tee /etc/dnsmasq.conf > /dev/null << EOD
+mv /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
+cat << EOD > /etc/dnsmasq.conf
 interface=wlan0
   dhcp-range=192.168.3.15,192.168.3.92,255.255.255.0,24h
 EOD
@@ -36,9 +39,9 @@ EOD
 # Configure hostapd
 
 if [ -f /etc/hostapd/hostapd.conf ]; then
-  sudo mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bak
+  mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bak
 fi
-sudo tee /etc/hostapd/hostapd.conf > /dev/null << EOD
+cat << EOD > /etc/hostapd/hostapd.conf
 interface=wlan0
 driver=nl80211
 ssid=PortPi
@@ -55,21 +58,54 @@ wpa_pairwise=TKIP
 rsn_pairwise=CCMP
 EOD
 
-sudo sed -i 's/^\#\?DAEMON_CONF=.*/DAEMON_CONF="\/etc\/hostapd\/hostapd\.conf"/' /etc/default/hostapd
+sed -i 's/^\#\?DAEMON_CONF=.*/DAEMON_CONF="\/etc\/hostapd\/hostapd\.conf"/' /etc/default/hostapd
 
 # Starting services
 
-sudo systemctl start hostapd
-sudo systemctl start dnsmasq
+systemctl start hostapd
+systemctl start dnsmasq
 
 # Allow Ethernet internet
 
-sudo sed -i 's/^\#net\.ipv4\.ip_forward=1/net\.ipv4\.ip_forward=1/' /etc/sysctl.conf
-sudo iptables -t nat -A  POSTROUTING -o eth0 -j MASQUERADE
-sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+sed -i 's/^\#net\.ipv4\.ip_forward=1/net\.ipv4\.ip_forward=1/' /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
+
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables-save > /etc/iptables.ipv4.nat
 if ! grep -q '/etc/iptables.ipv4.nat' /etc/rc.local; then
-  sudo sed -i 's/exit 0/iptables-restore < \/etc\/iptables\.ipv4\.nat/' /etc/rc.local
+  sed -i 's/exit 0/iptables-restore < \/etc\/iptables\.ipv4\.nat/' /etc/rc.local
   echo >> /etc/rc.local
   echo 'exit 0' >> /etc/rc.local
 fi
 
+# Install PortPi
+
+mkdir /home/pi/PortPi
+cd /home/pi/PortPi
+
+git clone https://github.com/portpi/server.git
+cd server
+npm install
+cd -
+
+git clone https://github.com/portpi/server-startup.git
+cp server-startup/init.d/portpi-server /etc/init.d/
+rm -rf server-startup
+
+chown -R pi.pi .
+
+/etc/init.d/portpi-server start
+update-rc.d portpi-server defaults
+
+cat << EOD
+==================================================
+
+      PortPi is installed successfully!
+
+You can connect by joining AP - PortPi
+                          with password portpi.com
+
+Visit http://192.168.3.14:15926/ afterwards.
+
+==================================================
+EOD
